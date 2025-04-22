a3ce1d8d0a88c5ea620e001c1cbd017238cf94ef
dharmik-gandha <dharmikgandha9@gmail.com>
2025-04-22 20:51:05 +0530

    Updated app.py for Render deployment

--- app.py
+++ app.py
@@ -24,5 +24,7 @@ def home():
         articles = news_data.get("articles", [])
     return render_template('index.html', articles=articles)
 
+# This part is updated for Render
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from environment or default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
